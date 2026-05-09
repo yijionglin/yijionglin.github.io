014074787f7a16cbb2ff45d4c6c4e3a25540ba8e
Yijiong Lin <th20988@bristol.ac.uk>
2026-05-10 00:07:53 +0100

    update

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -11,8 +11,13 @@ module ExternalPosts
       if site.config['external_sources'] != nil
         site.config['external_sources'].each do |src|
           p "Fetching external posts from #{src['name']}:"
-          xml = HTTParty.get(src['rss_url']).body
-          feed = Feedjira.parse(xml)
+          begin
+            xml = HTTParty.get(src['rss_url']).body
+            feed = Feedjira.parse(xml)
+          rescue StandardError => e
+            Jekyll.logger.warn "External posts:", "Skipping #{src['name']} (#{e.message})"
+            next
+          end
           feed.entries.each do |e|
             p "...fetching #{e.url}"
             slug = e.title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
